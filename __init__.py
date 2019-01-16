a0181e7951fcb245ff2979a4760567ab08a7c83c
olejar <milan.olejar@sopsr.sk>
2019-01-16 07:27:55 +0100

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 """
 /***************************************************************************
  Vgi2ShpConverter
@@ -8,20 +7,10 @@
         begin                : 2014-06-20
         copyright            : (C) 2014 by Ing.Olejar Milan, State Nature Conservancy, Slovakia
         email                : milan.olejar@sopsr.sk
- ***************************************************************************/
-
-/***************************************************************************
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- ***************************************************************************/
- This script initializes the plugin, making it known to QGIS.
+***************************************************************************/
 """
 
 def classFactory(iface):
     # load Vgi2ShpConverter class from file Vgi2ShpConverter
-    from vgi2shpconverter import Vgi2ShpConverter
+    from Vgi2ShpConverter.vgi2shpconverter import Vgi2ShpConverter
     return Vgi2ShpConverter(iface)
